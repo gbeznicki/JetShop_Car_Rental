37710c43dd6db0cf7db714920ddc7cad7216086b
Gustaw Beźnicki <gustaw.beznicki@bakk.com>
2021-06-10 20:23:27 +0200

    Added IVehicle interface and created separate project for interfaces

---
+++ CarRental.Interfaces/IVehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Interfaces
+{
+    public interface IVehicle
+    {
+        bool Rent();
+        string GetReservationNumber();
+        decimal Return();
+    }
+}
