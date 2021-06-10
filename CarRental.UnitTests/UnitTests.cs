7d0243df624b29f79746022a412e898dd3e90f9f
Gustaw Beźnicki <gustaw.beznicki@bakk.com>
2021-06-10 20:36:14 +0200

    Added typeof tests

--- CarRental.UnitTests/UnitTests.cs
+++ CarRental.UnitTests/UnitTests.cs
@@ -1,19 +1,43 @@
+using CarRental.BusinessLogic.Vehicles;
+using CarRental.Interfaces;
 using NUnit.Framework;
 
 namespace CarRental.UnitTests
 {
+    [TestFixture]
     public class Tests
     {
-        IVehicle vehicle;
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void TestTypeofCompact()
         {
-            Assert.Pass();
+            IVehicle vehicle = new Compact();
+            Assert.IsInstanceOf(typeof(Compact), vehicle);
+            Assert.IsNotInstanceOf(typeof(Minivan), vehicle);
+            Assert.IsNotInstanceOf(typeof(Premium), vehicle);
         }
+
+        [Test]
+        public void TestTypeofMinivan()
+        {
+            IVehicle vehicle = new Minivan();
+            Assert.IsInstanceOf(typeof(Minivan), vehicle);
+            Assert.IsNotInstanceOf(typeof(Compact), vehicle);
+            Assert.IsNotInstanceOf(typeof(Premium), vehicle);
+        }
+
+        [Test]
+        public void TestTypeofPremium()
+        {
+            IVehicle vehicle = new Premium();
+            Assert.IsInstanceOf(typeof(Premium), vehicle);
+            Assert.IsNotInstanceOf(typeof(Minivan), vehicle);
+            Assert.IsNotInstanceOf(typeof(Compact), vehicle);
+        }
+
     }
 }
